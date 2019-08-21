787b095a03857672fca88b3cb8506cbd8d05fbe5
aico0118 <aa00547386@gmail.com>
2019-08-21 12:09:59 +0800

    test
    test

--- sendbot/script.js
+++ sendbot/script.js
@@ -10,15 +10,7 @@ var app = new Vue({
   el: "#app",
   methods:{
     sendMsg(){
-      alert('a');
-      alert(liff);
-      liff.sendMessages([
-        {
-          type:'text',
-          text:'Hello, World!1'
-        }
-      ]);
-      eval( 'let msg = ' +$('#message').val()+';');
+      eval( 'var msg = ' +$('#message').val()+';');    
       liff.sendMessages(msg);
 
     }
